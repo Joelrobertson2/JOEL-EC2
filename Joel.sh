84b90dcf258f5c08eb4a08e13a825b98cec1061d
Joelr <Joelrobertson33@gmail.com>
2024-02-06 11:14:56 -0600

    FIX: "Updated Code to Output HostNAME & added Commit Statement"
      Contributing-to: https://wemadevops.atlassian.net/jira/software/projects/DEV/boards/2?selectedIssue=DEV-8
    
    Problem
      # Problem, Task, Reason for Commit
    
    Solution
      # Solution or List of Changes
     Note

--- Joel.sh
+++ Joel.sh
@@ -3,24 +3,9 @@
 sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
-sudo systemctl start httpd
+sudo service httpd start
 
 your_name="Joel Robertson"
-
-sudo tee /var/www/html/index.html > /dev/null <<EOF
-<h1>Welcome to $your_name Website from \$(hostname -f)</h1>
-EOF
-
-sudo mkdir -p /var/www/html/app1
-
-sudo tee /var/www/html/app1/index.html > /dev/null <<EOF
-<!DOCTYPE html>
-<html>
-<body style="background-color:rgb(250, 210, 210);">
-  <h1>Welcome to $your_name Website from \$(hostname -f)!</h1>
-  <p>Ec2 Demo</p>
-  <p>Application Version: V1</p>
-</body>
-</html>
-EOF
-
+sudo echo "<h1>Welcome to $your_name Website from $(hostname -f)</h1>" | sudo tee /var/www/html/index.html
+sudo mkdir /var/www/html/app1
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to $your_name Website from $(hostname -f)!</h1> <p>Ec2 Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
